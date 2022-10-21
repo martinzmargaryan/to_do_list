e5b6fcef7d877ba0479f7a2b0ad490030fb611de
martinzmargaryan <martinzmargaryan@gmail.com>
2022-10-21 13:07:02 +0400

    commit todoForm

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
-import React from "react";
-import ButtonAppBar from "./components/tets";
+import { Container } from "@mui/system";
+import React, { useState } from "react";
+
+import TodoForm from "./components/TodoForm";
 
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      title: "Play",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      title: "Play tetris",
+      isCompleted: true,
+    },
+  ]);
+
+  const addTodo = (text) => {
+    const newTodo = {
+      id: Math.random(),
+      title: text,
+      isCompleted: false,
+    };
+    setTodos([...todos, newTodo]);
+  };
+
   return (
-    <div>
-      <ButtonAppBar />
-    </div>
+    <Container maxWidth="sm">
+      <TodoForm addTodo={addTodo} />
+    </Container>
   );
 }
 
